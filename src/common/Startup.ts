d4c82e1604320f92e8e56b90448ba1cc845baf54
ehsan <47655253+cloner75@users.noreply.github.com>
2023-03-25 00:41:28 +0330

    update startup service

--- src/common/Startup.ts
+++ src/common/Startup.ts
@@ -1,33 +1,63 @@
-import { IStartup } from './interfaces/startup';
 import * as express from 'express';
 import * as cors from 'cors';
 import * as cookieParser from 'cookie-parser';
 
+import { IStartup } from './interfaces/startup';
+import TelegramService from './TelegramService';
 
 
 export default class StartUp implements IStartup {
   app: express;
 
   constructor(app: express) {
-    this.app = app;
+    this.app = app();
   }
 
   setConnections(): void {
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(express.json());
+
+    this.app.use(cors({
+      origin: '*',
+    }));
+    this.app.use(cookieParser());
 
   }
 
   setRouters(): void {
-    this.app.use(cors());
-    this.app.use(cookieParser());
+    this.app.post(`/bot`, (req, res) => {
+      return res.send(Object.keys(req));
+    });
+
+    this.app.use((_req, res) => {
+      return res.status(404).send('Not Found Route');
+    });
+  }
+
+  processHandler() {
+    process.on('rejectionHandled', (error) => {
+      console.log('rejectionHandler => ', error);
+    });
+
+    process.on('uncaughtException', (error) => {
+      console.log('uncaughtException => ', error);
+    });
+  }
+
+  additionalServices() {
+    const telegram = new TelegramService();
+    // telegram.startWebhook()
+    telegram.startGetUpdate();
   }
 
   startService(): void {
     this.setConnections();
+    this.processHandler();
     this.setRouters();
+    this.additionalServices();
 
-    this.app.listen(process.env.PORT || 3000, () => {
+    this.app.listen(process.env.PORT || 4000, () => {
       console.log('app runing on port  :', process.env.PORT || 4000);
     });
   }
-
 }
