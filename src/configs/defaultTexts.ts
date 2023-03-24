3eacc16b4cf2c889d4e26d77b112e9e17cb566b4
ehsan <47655253+cloner75@users.noreply.github.com>
2023-03-25 00:41:43 +0330

    ::init:: default text usage of bot

---
+++ src/configs/defaultTexts.ts
@@ -0,0 +1,19 @@
+
+export enum telegramTexts {
+  start = 'سلام به ربات گفتگوی رایگان خوش آمدید',
+  wrong = 'گزینه ی ارسالی صحیح نیست . لطفا دقت کنید',
+  keyboardKeyProfile = 'تکمیل پروفایل',
+  keyboardKeyChat = 'شروع چت',
+  boy = 'پسر',
+  girl = 'دختر',
+  age = 'سن',
+  city = 'شهر',
+  allCities = 'تمامی شهرها',
+  startSearch = 'شروع جستجو',
+  picture = 'تصویر',
+  name = 'نام',
+  showBio = 'نمایش پروفایل',
+  startConversation = 'شروع مکالمه',
+};
+
+
